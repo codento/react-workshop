4e8a534353975b232e0439412560997270607193
tapir79 <saara.m.pakarinen@gmail.com>
2019-03-18 22:07:06 +0200

    app calc

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <div className='section' style={{ height: 'calc(100vh - 300px)'}}>
+      <div className='section' style={{ height: 'calc(100vh - 300px)' }}>
         <p> Hei maailma! </p>
       </div>
       <Footer />
