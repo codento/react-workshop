597e8a0f20c195fa7e7b45622e6ca6a95663f51a
woltsu <oltsuwh@gmail.com>
2019-03-18 15:09:11 +0200

    added workshop project

---
+++ workshop/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './Button';
+
+const Footer = () => {
+  return (
+    <div>
+      <Button text='Kyllä' />
+      <Button text='Ei' />
+    </div>
+  )
+}
+
+export default Footer;
