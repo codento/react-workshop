4b8cb40ed1471619da28b5a756c79965d5a24870
woltsu <oltsuwh@gmail.com>
2019-03-18 20:06:30 +0200

    changed app to functional

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <div className='section' style={{ height: 'calc(100vh - 300px)'}}>
-        Hei maailma!</div>
-        <Footer />
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <div className='section' style={{ height: 'calc(100vh - 300px)'}}>
+        <p>Hei maailma!</p>
       </div>
-    )
-  }
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
