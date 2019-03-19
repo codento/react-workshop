4730b7bad3d4be03b8b00141799c1cafb3027d97
tapir79 <saara.m.pakarinen@gmail.com>
2019-03-19 08:00:04 +0200

    footer

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -10,12 +10,7 @@ const App = () => {
       <div className="section" style={{ height: "calc(100vh - 300px)" }}>
         <p> Hei maailma! </p>
       </div>
-      {/* <Footer /> */}
-
-<footer className="footer">
-  <div>Some Button 1</div>
-  <div>Some Button 2</div>
-</footer>
+      <Footer />
     </div>
 
 
