e4137edbef07432baa92e761e1ea43530f03031a
woltsu <oltsuwh@gmail.com>
2019-03-20 14:24:59 +0200

    added image

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Counter from './Counter'
 
+const apiUrl = 'https://koira-api.herokuapp.com/api/v1/dogs'
+
 const App = () => {
+  const [ dogs, setDogs ] = useState([])
+
+  const fetchDogs = async () => {
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    setDogs(data)
+  }
+
+  useEffect(() => {
+    fetchDogs()
+  }, [])
+
   return (
     <div className="container">
       <Header />
       <div className='section' style={{ height: 'calc(100vh - 300px)'}}>
-        <p>Hei maailma!</p>
+        <Counter />
       </div>
       <Footer />
     </div>
