034db5aa03b19b1499dd439b610b253b9dbc5d42
eevajonnapanula <eeva-jonna.panula@codento.com>
2019-03-22 08:19:45 +0200

    Harjoitus 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>Hei maailma!</p>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <p>Hei maailma!</p>
+    </div>
+  )
 }
 
 export default App;
