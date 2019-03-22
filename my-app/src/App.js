506a98d9d12b3a60be19575c16d34caee1921885
eevajonnapanula <eeva-jonna.panula@codento.com>
2019-03-22 11:43:56 +0200

    Harjoitus 6

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
 const App = () => {
+  const [ pets, setPets ] = useState([])
   return (
     <div className="container">
       <Header />
