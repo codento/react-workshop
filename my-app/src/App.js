e7a02bd3be343be91b5e698f8d210c51ef9921b2
eevajonnapanula <eeva-jonna.panula@codento.com>
2019-03-22 08:32:17 +0200

    Harjoitus 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <p>Hei maailma!</p>
+      <Footer />
     </div>
   )
 }
